b6ae85f67902f5d1cde028f2092d6cc5f1f38ce8
xerxes1986 <xerxes1986@gmail.com>
2014-05-14 12:52:44 -0500

    Updating flat systematics to apply scale/matching flat uncertainties to all categories except wlight 0top1btag and 0top0btag where we have enough statistics to use the shape systematic

--- theta/analysis.py
+++ theta/analysis.py
@@ -84,13 +84,14 @@ def build_boosted_semileptonic_model(files, filter, signal, mcstat, eflag=False,
         #    model.add_asymmetric_lognormal_uncertainty(sys, -math.log( _MINUS_ ), math.log( _PLUS_ ), proc, obs)
         #  Where _PLUS_ = proc_sys_plus.Integral() / proc.Integral()  after the Chi2 selection
         #  Where _MINUS_ = proc_sys_minus.Integral() / proc.Integral()  after the Chi2 selection
-        for obs in ['el_1top0btag_mttbar','el_1top1btag_mttbar','el_1top2btag_mttbar','el_0top2btag_mttbar']:
+        for obs in ['el_1top0btag_mttbar','el_1top1btag_mttbar','el_1top2btag_mttbar','el_0top0btag_mttbar','el_0top1btag_mttbar','el_0top2btag_mttbar']:
             model.add_asymmetric_lognormal_uncertainty('scale_vjets', -math.log(1.49389), math.log(0.50611), 'wb', obs)
             model.add_asymmetric_lognormal_uncertainty('matching_vjets', -math.log(1.01370), math.log(0.98630), 'wb', obs)
 
             model.add_asymmetric_lognormal_uncertainty('scale_vjets', -math.log(1.49901), math.log(0.50099), 'wc', obs)
             model.add_asymmetric_lognormal_uncertainty('matching_vjets', -math.log(0.98011), math.log(1.01989), 'wc', obs)
 
+        for obs in ['el_1top0btag_mttbar','el_1top1btag_mttbar','el_1top2btag_mttbar','el_0top2btag_mttbar']:
             model.add_asymmetric_lognormal_uncertainty('scale_vjets', -math.log(1.50848), math.log(0.49152), 'wlight', obs)
             model.add_asymmetric_lognormal_uncertainty('matching_vjets', -math.log(0.97089), math.log(1.02911), 'wlight', obs)
 
